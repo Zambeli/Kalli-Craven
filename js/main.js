db991bccd0bf4760b0c4750acd8688c51bf6009a
Zambeli <zambelidev@gmail.com>
2023-09-13 19:00:55 -0300

    finalizando para pc

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-const voltar = document.querySelector('#inicio')
-const termos = document.querySelector('#terms')
+const audioEaster = new Audio ('../src/easterEgg.mp3')
+const easterEgg = document.querySelector('#easteregg')
 
-termos.addEventListener('click', termoss)
-
-voltar.addEventListener('click', inicio)
-
-function termoss () {
-    window.location = '../termos.html'
-}
-
-function inicio () {
-    window.location = './index.html'
-}
+easterEgg.addEventListener('click', () => {
+    audioEaster.play()
+    easterEgg.volume = 0.2
+})
